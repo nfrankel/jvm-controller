c5a420301a276b24b50aba45a18a7fed79900e17
Nicolas Frankel <nicolas@frankel.ch>
2019-12-08 20:21:08 +0100

    Loop over the main code

--- src/main/java/ch/frankel/kubernetes/extend/ListPods.java
+++ src/main/java/ch/frankel/kubernetes/extend/ListPods.java
@@ -1,5 +1,6 @@
 package ch.frankel.kubernetes.extend;
 
+import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.Configuration;
 import io.kubernetes.client.openapi.apis.CoreV1Api;
 import io.kubernetes.client.util.Config;
@@ -11,9 +12,16 @@ public class ListPods {
     private static final Logger LOGGER = LoggerFactory.getLogger(ListPods.class);
 
     public static void main(String[] args) throws Exception {
-        LOGGER.info("*** JVM Operator v1.1 ***");
+        LOGGER.info("*** JVM Operator v1.2 ***");
         var client = Config.defaultClient();
         Configuration.setDefaultApiClient(client);
+        while (true) {
+            print();
+            Thread.sleep(1000);
+        }
+    }
+
+    private static void print() throws ApiException {
         var core = new CoreV1Api();
         var pods = core.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null, null);
         pods.getItems()
