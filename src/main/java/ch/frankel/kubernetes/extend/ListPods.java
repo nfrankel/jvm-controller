f6d017c7f16f1e5ccf8bc42c3a4faf4a3c6a6767
Nicolas Frankel <nicolas@frankel.ch>
2019-12-08 16:45:56 +0100

    Allow container to communicate with the server API
      Create role, service account and role binding
      Configure pod to use all of the above

--- src/main/java/ch/frankel/kubernetes/extend/ListPods.java
+++ src/main/java/ch/frankel/kubernetes/extend/ListPods.java
@@ -5,17 +5,22 @@ import io.kubernetes.client.Configuration;
 import io.kubernetes.client.apis.CoreV1Api;
 import io.kubernetes.client.models.V1PodList;
 import io.kubernetes.client.util.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ListPods {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ListPods.class);
+
     public static void main(String[] args) throws Exception {
+        LOGGER.info("*** JVM Operator v1.0 ***");
         ApiClient client = Config.defaultClient();
         Configuration.setDefaultApiClient(client);
         CoreV1Api core = new CoreV1Api();
         V1PodList pods = core.listPodForAllNamespaces(null, null, null, null, null, null, null, null);
         pods.getItems()
                 .stream()
-                .map(it -> "- " + it.getMetadata().getName())
-                .forEach(System.out::println);
+                .map(it -> "* " + it.getMetadata().getName())
+                .forEach(LOGGER::info);
     }
 }
