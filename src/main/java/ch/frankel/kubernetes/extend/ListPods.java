d978541bed08c507312886a8bbfbe61e9516f4d2
Nicolas Frankel <nicolas@frankel.ch>
2019-12-09 09:19:50 +0100

    Refactor to watch over pod changes instead of polling

--- src/main/java/ch/frankel/kubernetes/extend/ListPods.java
+++ src/main/java/ch/frankel/kubernetes/extend/ListPods.java
@@ -1,34 +1,50 @@
 package ch.frankel.kubernetes.extend;
 
+import com.google.gson.reflect.TypeToken;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.OkHttpClient;
 import io.kubernetes.client.ApiClient;
 import io.kubernetes.client.ApiException;
 import io.kubernetes.client.Configuration;
 import io.kubernetes.client.apis.CoreV1Api;
-import io.kubernetes.client.models.V1PodList;
+import io.kubernetes.client.models.V1Node;
 import io.kubernetes.client.util.Config;
+import io.kubernetes.client.util.Watch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.StreamSupport;
 
 public class ListPods {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ListPods.class);
 
     public static void main(String[] args) throws Exception {
-        LOGGER.info("*** JVM Operator v1.1 ***");
+        LOGGER.info("*** JVM Operator v1.2 ***");
         ApiClient client = Config.defaultClient();
         Configuration.setDefaultApiClient(client);
-        while (true) {
-            print();
-            Thread.sleep(1000);
+        configureTimeout(client);
+        print(client);
+    }
+
+    private static void print(ApiClient client) throws ApiException, IOException {
+        CoreV1Api api = new CoreV1Api();
+        Call call = api.listPodForAllNamespacesCall(null, null, null, null, null, null, null, Boolean.TRUE, null, null);
+        Type type = new TypeToken<Watch.Response<V1Node>>() {
+        }.getType();
+        try (Watch<V1Node> watch = Watch.createWatch(client, call, type)) {
+            StreamSupport.stream(watch.spliterator(), false)
+                    .map(it -> it.type + ": " + it.object.getMetadata().getName())
+                    .forEach(LOGGER::info);
         }
     }
 
-    private static void print() throws ApiException {
-        CoreV1Api core = new CoreV1Api();
-        V1PodList pods = core.listPodForAllNamespaces(null, null, null, null, null, null, null, null);
-        pods.getItems()
-                .stream()
-                .map(it -> "* " + it.getMetadata().getName())
-                .forEach(LOGGER::info);
+    private static void configureTimeout(ApiClient client) {
+        OkHttpClient httpClient = client.getHttpClient();
+        httpClient.setReadTimeout(0, TimeUnit.SECONDS);
+        client.setHttpClient(httpClient);
     }
 }
